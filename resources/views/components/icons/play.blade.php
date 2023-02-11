d7bcc1642423135bb5f521206d7b8747b7cdf7fe
Mark Leidman <marksleidmans@flynowpaylater.com>
2023-02-11 19:44:37 +0200

    play circle

--- resources/views/components/icons/play.blade.php
+++ resources/views/components/icons/play.blade.php
@@ -1,17 +1,4 @@
-<svg width="138" height="138" viewBox="0 0 138 138" fill="none" xmlns="http://www.w3.org/2000/svg">
-<g filter="url(#filter0_d_1945_6485)">
-<circle cx="63" cy="63" r="36" fill="#2AA84A"/>
-<path d="M81 63L54 78.5885L54 47.4115L81 63Z" fill="white"/>
-</g>
-<defs>
-<filter id="filter0_d_1945_6485" x="0" y="0" width="138" height="138" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
-<feFlood flood-opacity="0" result="BackgroundImageFix"/>
-<feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha"/>
-<feOffset dx="6" dy="6"/>
-<feGaussianBlur stdDeviation="16.5"/>
-<feColorMatrix type="matrix" values="0 0 0 0 0.164706 0 0 0 0 0.658824 0 0 0 0 0.290196 0 0 0 0.59 0"/>
-<feBlend mode="normal" in2="BackgroundImageFix" result="effect1_dropShadow_1945_6485"/>
-<feBlend mode="normal" in="SourceGraphic" in2="effect1_dropShadow_1945_6485" result="shape"/>
-</filter>
-</defs>
+<svg width="390" height="390" viewBox="0 0 390 390" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <circle cx="195" cy="195" r="36" fill="#2AA84A"/>
+    <path d="M213 195L186 210.588L186 179.412L213 195Z" fill="white"/>
 </svg>
